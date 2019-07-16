c7ebcf31c3b75d2c7402a85574eb20b9fbb15730
Ruth Oldja <ROldja@foundationsoft.com>
2019-07-16 17:01:11 -0400

    added data.js file

--- loggedin.js
+++ loggedin.js
@@ -1,11 +1,27 @@
-'use strict';
-
-// import trainingData from './hangout_recommendation';
-
 $(() => {
 
-    // for (let place of trainingData) {
-    //     console.log(place);
-    // }
+    $('#submit').click(function() {
+        let numberOfPeople = $('#people').val();
+        let amountOfTime = $('#hours').val();
+        let food = $('#food').is(':checked') ? 1 : 0;
+        let coffee = $('#coffee').is(':checked') ? 1 : 0;
+        let iceCream = $('#ice_cream').is(':checked') ? 1 : 0;
+        // ADD PLACE NAME DROP-DOWN
+        if (numberOfPeople > 0 && numberOfPeople <= 20 && amountOfTime > 0 && amountOfTime <= 3) {
+            trainingData.push({
+                input : {
+                numberOfPeople : normalizePeople(numberOfPeople),
+                amountOfTime : normalizeHours(amountOfTime),
+                food : food,
+                coffee : coffee,
+                iceCream : iceCream,
+            }, output : { "Strickland's" : 1 } },);
+            // confirm success
+            clearForm();
+        }
+        else {
+            $('#error').css('display', 'block');
+        }
+    });
 
 });
